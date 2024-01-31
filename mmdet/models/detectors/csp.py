07217972ef9639755036712e0018b62b51a867f2
Abdul Hannan Khan <khan.abdulhannan777@gmail.com>
2024-01-31 17:20:38 +0100

    Fixed acc metric bug

--- mmdet/models/detectors/csp.py
+++ mmdet/models/detectors/csp.py
@@ -203,8 +203,8 @@ class CSP(SingleStageDetector):
                     if error is not None:
                         mid_array.append(error.item())
                 else:
-                    pred_tti_value = pred_tti_value.item()
-                    gt_tti_value = gt_tti_value.item()
+                    pred_tti_value = pred_tti_value.item() > 0.5
+                    gt_tti_value = gt_tti_value.item() > 0.5
                     eval = np.abs(pred_tti_value - gt_tti_value)*100
                 if eval is not None:
                     if gt_labels is None:
