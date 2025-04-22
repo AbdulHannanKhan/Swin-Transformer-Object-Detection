5ee6932c7cc5cc702a49bd5419002476ebe5379a
Abdul Hannan Khan <khan.abdulhannan777@gmail.com>
2025-04-22 12:55:33 +0200

    resolved socket clash issue

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -2,8 +2,12 @@
 
 CONFIG=$1
 GPUS=$2
-PORT=${PORT:-29506}
+
+# Find an available port using a Python one-liner
+PORT=$(python -c "import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.bind(('', 0)); print(s.getsockname()[1]); s.close()")
+
+echo "Using PORT: $PORT"
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
-CUDA_VISIBLE_DEVICES=4,5,6,7 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
     $(dirname "$0")/train.py $CONFIG --launcher pytorch ${@:3}
